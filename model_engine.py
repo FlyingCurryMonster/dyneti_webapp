c782ba27710483724ab2dd16f8a342ec0fc5bdea
FlyingCurryMonster <rakinb1998@gmail.com>
2026-05-20 19:32:04 -0400

    ModelEngine class

--- model_engine.py
+++ model_engine.py
@@ -1,69 +1,84 @@
 MODEL_PATH = './model.tflite'
 
-from email.mime import image
+import os
 
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+os.environ["MPLCONFIGDIR"] = "/tmp"
+import warnings
+warnings.filterwarnings("ignore", message=".*tf.lite.Interpreter is deprecated.*")
 import numpy as np
 from tensorflow import lite as tf_lite
 from PIL import Image
 
-def load_model():
-    # Load TFLite model and allocate tensors.
-    interpreter = tf_lite.Interpreter(model_path=MODEL_PATH)
-    interpreter.allocate_tensors()
-    return interpreter
+class ModelEngine:
+    def __init__(self, model_path=MODEL_PATH):
+        self.model_path = model_path
+        self.interpreter = self.load_model()
 
+        if len(self.interpreter.get_input_details()[0]['shape']) == 4:
+            height, width = self.interpreter.get_input_details()[0]['shape'][1:3]
+            self.input_img_shape = (height, width)
+        else:
+            raise ValueError("Can only hanlde models with input shapes of len 4 (batch_size, height, width, channels)") 
 
-def preprocess_input(
-        # image: Image,
+        self.input_dtype = self.interpreter.get_input_details()[0]['dtype']
+   
+
+    def predict(self, input: np.ndarray):
+        '''
+        Outputs probability of cat
+        '''
+        # assert that the input matches what the interpreter expects
+        assert input.shape == tuple(self.interpreter.get_input_details()[0]['shape']), "Input shape does not match model input specification"
+        
+        self.interpreter.set_tensor(self.interpreter.get_input_details()[0]['index'], input)
+        self.interpreter.invoke()
+        
+        return self.interpreter.get_tensor(self.interpreter.get_output_details()[0]['index'])
+
+    def load_model(self):
+        # Load TFLite model and allocate tensors.
+        interpreter = tf_lite.Interpreter(model_path=self.model_path)
+        interpreter.allocate_tensors()
+        return interpreter
+    
+    def preprocess_input(
+        self,
         img_path: str,
-        shape: tuple,
-        rescale: bool = True        
+        rescale: bool = True
         ):
-    img = Image.open(img_path).resize(shape)
-    img_resize = img.resize(shape)
-    img_array = np.array(img_resize, dtype=np.float32) # get rid of dtype hardcode
-    img_array = np.expand_dims(img_array, axis=0) # add batch dimension
-    if rescale:
-        img_array /= 255.0 # scale to [0, 1], check if PIL can figure out what the scale of the image is
-    return img_array
+        def pil_scale_value(img):
+            if img.mode in ("RGB", "RGBA", "L", "P"):
+                return 255.0
+            if img.mode in ("I;16", "I;16L", "I;16B"):
+                return 65535.0
+            if img.mode == "1":
+                return 1.0
+            if img.mode == "F":
+                return None  # already float-ish; don't know intended scale
+            raise ValueError(f"Unsupported image mode: {img.mode}")
+
+        img = Image.open(img_path).resize(self.input_img_shape)
+        img_resize = img.resize(self.input_img_shape)
+        img_array = np.array(img_resize, dtype=self.input_dtype) # get rid of dtype hardcode
+        img_array = np.expand_dims(img_array, axis=0) # add batch dimension
+        if rescale:   
+            scale_val = pil_scale_value(img) if pil_scale_value(img) is not None else max(img_array.max(), 1.0) # avoid divide by zero
+            img_array /= scale_val # scale to [0, 1]
+
+        return img_array
     
-
-def predict(input:np.ndarray, interpreter: tf_lite.Interpreter):
-    '''
-    Outputs probability of cat
-    '''
-    # assert that the input matches what the interpreter expects
-    assert input.shape == interpreter.get_input_details()[0]['shape'], "Input shape does not match model input specification"
+if __name__ == "__main__":
+    model_engine = ModelEngine()
+    # test cat image output
     
-    interpreter.set_tensor(interpreter.get_input_details()[0]['index'], input)
-    interpreter.invoke()
-    
-    return interpreter.get_tensor(interpreter.get_output_details()[0]['index'])
+    input = model_engine.preprocess_input('./cat.jpg')
+    output = model_engine.predict(input)
+    print(f"Cat probability: {output[0][0]:.4f}")
 
-interpreter = load_model()
-model_img_shape = interpreter.get_input_details()[0]['shape'][1:3] # get the expected input shape (height, width)
-input = preprocess_input(img_path='cat.jpg', shape=model_img_shape)
+    # test dog image output
+    input = model_engine.preprocess_input('./dog2.webp')
+    output = model_engine.predict(input)
+    print(f"Cat probability: {output[0][0]:.4f}")
 
-model_out = predict(input, interpreter)
-print(model_out)
-
-# cat_img = Image.open('cat.jpg')
-# cat_img_resize = cat_img.resize((224, 224))
-# cat_input = np.array(cat_img_resize, dtype=np.float32) / 255
-# cat_input = np.expand_dims(cat_input, axis=0)
-# # Load TFLite model and allocate tensors.
-
-# # Get input and output tensors.
-# input_details = interpreter.get_input_details()
-# output_details = interpreter.get_output_details()
-
-# # Test model on random input data.
-# input_shape = input_details[0]['shape']
-# input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-# interpreter.set_tensor(input_details[0]['index'], input_data)
-
-# interpreter.invoke()
-
-# # The function `get_tensor()` returns a copy of the tensor data.
-# # Use `tensor()` in order to get a pointer to the tensor.
-# output_data = interpreter.get_tensor(output_details[0]['index'])
